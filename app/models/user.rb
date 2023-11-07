592db1ff15bf0f8356660df7c59e93c21e16eb5c
Romil <romil@directshifts.com>
2023-11-07 08:31:39 +0530

    Add count_hits method back

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :hits
+
+  # We can remove this method but kept it considering we are using this at other places
+  def count_hits
+    Rails.cache.read("#{self.id}_hits_count") || 0
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_many(:hits) }
+
+  describe '#count_hits' do
+    it 'returns hits count' do
+    end
+
+    it 'returns 0 in case of no hits' do
+    end
+  end
 end
